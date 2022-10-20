bd828b3a4f49105f375433d81098dadf3da104a7
dmarsan24 <dmarsan2409@g.educaand.es>
2022-10-20 12:56:13 +0200

    Prueba git

---
+++ Assets/Scripts/ControlConexion.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class ControlConexion : MonoBehaviourPunCallbacks
+{
+    public void ConectarServidor()
+    {
+        Debug.Log("Se ha pulsado el botón");
+    }
+
+}
